cb500598e537bdca418ac598b9b81a39ec69a965
theeamirkhan <theeamirkhan@gmail.com>
2026-04-11 11:54:57 -0400

    Update page.tsx

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
-      <SignIn />
+      <SignIn forceRedirectUrl="/cockpit" fallbackRedirectUrl="/cockpit" />
     </div>
   )
 }
